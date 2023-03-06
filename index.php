5b42aa3f0e0de764e9a336a14725730dbd5c35e4
Alehernandezt <j.alehernandezt@gmail.com>
2023-03-06 14:13:21 -0400

    ahora si?

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-commerce</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <a href="#" class="logo">LOGO</a>
+        <div class="group">
+            <ul class="navigation">
+                <li><a href="#">Iniciar Sesión</a></li>
+                <li><a href="#">Explorar</a></li>
+                <li><a href="#">Ayuda</a></li>
+                <li><a href="#">Contactanos</a></li>
+            </ul>
+            <div class="search">
+                <span class="icon">
+                    <ion-icon name="search-outline" class="searchBtn"></ion-icon>
+                    <ion-icon name="close-outline" class="closeBtn"></ion-icon>
+                </span>
+            </div>
+            <ion-icon name="menu-outline"class="menuToggle"></ion-icon>
+        </div>
+        <div class="searchBox">
+            <input type="text" placeholder="Buscar">
+        </div>
+    </header>
+    <main>
+
+    </main>
+
+
+    <!-- link de iconos -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    
+    <!-- Link del js  -->
+    <script src="js/main.js"></script>
+
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,21 @@
+let searchBtn = document.querySelector('.searchBtn');
+let closeBtn = document.querySelector('.closeBtn');
+let searchBox = document.querySelector('.searchBox');
+let navigation = document.querySelector('.navigation');
+let menuToggle = document.querySelector('.menuToggle'); 
+let header = document.querySelector('.header');
+
+searchBtn.onclick = function(){
+    searchBox.classList.add('active');
+    closeBtn.classList.add('active');
+    searchBtn.classList.add('active');
+}
+
+closeBtn.onclick = function(){
+    searchBox.classList.remove('active');
+    closeBtn.classList.remove('active');
+    searchBtn.classList.remove('active');
+}
+menuToggle.onclick = function(){
+    header.classList.toggle('open');
+}
