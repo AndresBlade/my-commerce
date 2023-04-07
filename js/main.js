436e0179a465f7c7a58037b449c7580ba9a4993b
Alehernandezt <j.alehernandezt@gmail.com>
2023-04-07 16:13:58 -0400

    dashboard listo y añadi miPerfil en index

--- js/main.js
+++ js/main.js
@@ -28,3 +28,26 @@ menuToggle.onclick = function(){
     menuToggle.classList.remove('hide');
 }
 
+//      perfil
+const toggle = document.querySelector(".toggle")
+const menuDashboard = document.querySelector(".menu-dashboard")
+const iconoMenu = toggle.querySelector("i")
+const enlacesMenu = document.querySelectorAll(".enlace")
+
+toggle.addEventListener("click", () => {
+    menuDashboard.classList.toggle("open")
+
+    if(iconoMenu.classList.contains("bx-menu")){
+        iconoMenu.classList.replace("bx-menu", "bx-x")
+    }else {
+        iconoMenu.classList.replace("bx-x", "bx-menu")
+    }
+})
+
+enlacesMenu.forEach(enlace => {
+    enlace.addEventListener("click", () => {
+        menuDashboard.classList.add("open")
+        iconoMenu.classList.replace("bx-menu", "bx-x")
+    })
+})
+
