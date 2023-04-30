4094697565f81a3427e4569905552bf6a8c838ec
Andres Arrieche <andresarrieche11@gmail.com>
2023-04-30 11:54:20 -0400

    concebidos fetch para obtener usuarios y realizar registros

--- js/registro.js
+++ js/registro.js
@@ -5,20 +5,47 @@ btnLogin.addEventListener('click', e => {
 	e.preventDefault();
 
 	const user = {
-		nombre: document.querySelector('.userForm__name'),
-		email: document.querySelector('.userForm__email'),
-		contrasenna: document.querySelector('.userForm__password'),
+		nombre: document.querySelector('.userForm__name').value,
+		email: document.querySelector('.userForm__email').value,
+		contrasenna: document.querySelector('.userForm__password').value,
 		tipo_id: 2,
 	};
 
 	let response;
 
-	fetch('localhost:3000/api/user/register', {
-		method: 'POST',
-		mode: 'same-origin',
-		credentials: 'same-origin',
-		body: JSON.stringify(user),
-	}).then(respuesta => (response = respuesta));
+	console.log(user);
 
-	console.log(response);
+	fetch('http://127.0.0.1:3000/api/user/register', {
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(user),
+	})
+		.then(respuesta => respuesta.json())
+		.then(respuesta => console.log(respuesta))
+		.catch(err => console.log(err));
+
+	// fetch('http://127.0.0.1:3000/api/user/register', {
+	// 	method: 'POST',
+	// 	mode: 'cors',
+	// 	credentials: 'same-origin',
+	// 	headers: {
+	// 		'Content-Type': 'application/json',
+	// 	},
+	// 	body: JSON.stringify(user),
+	// })
+	// 	.then(respuesta => respuesta.json())
+	// 	.then(respuesta => console.log(respuesta))
+	// 	.catch(err => console.log(err));
+
+	// fetch('http://127.0.0.1:3000/api/user/users', {
+	// 	method: 'GET',
+	// 	url: 'http://127.0.0.1:3000',
+	// })
+	// 	.then(respuesta => (response = respuesta.json()))
+	// 	.then(respuesta => console.log(respuesta))
+	// 	.catch(err => console.log(err));
 });
