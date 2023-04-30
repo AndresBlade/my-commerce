9a4520340dfc13f0b7dca69b5a8c15d5bf9dbda1
Andres Arrieche <andresarrieche11@gmail.com>
2023-04-30 12:56:13 -0400

    agregadas funcionalidades de confimrar contrasenna, campos vacios y terminos y condiciones

--- js/registro.js
+++ js/registro.js
@@ -1,6 +1,42 @@
 'use strict';
 const btnLogin = document.querySelector('.btn-login');
 
+function mostrarMensajeError(msg) {
+	const cuadro = document.createElement('p');
+	const parent = document.querySelector('.userForm');
+
+	cuadro.classList.add('userForm__mensajeError');
+
+	cuadro.textContent = msg;
+
+	parent.append(cuadro);
+
+	setTimeout(() => {
+		cuadro.classList.add('userForm__mensajeError--fade');
+	}, 4400);
+
+	setTimeout(() => {
+		cuadro.remove();
+	}, 5000);
+}
+
+function validarCamposCorrectos() {
+	const form = document.querySelector('.userForm');
+
+	const camposRellenos = Array.from(form.children).every(input => {
+		if (input.tagName !== 'INPUT') return true;
+		return input.value !== '';
+	});
+
+	if (!camposRellenos)
+		return mostrarMensajeError('Faltan campos por rellenar');
+
+	const checkTerminos = document.querySelector('.termsCheckbox');
+
+	if (!checkTerminos.checked)
+		return mostrarMensajeError('Debe aceptar los términos y condiciones');
+}
+
 btnLogin.addEventListener('click', e => {
 	e.preventDefault();
 
@@ -11,9 +47,21 @@ btnLogin.addEventListener('click', e => {
 		tipo_id: 2,
 	};
 
+	validarCamposCorrectos();
+
+	// if (
+	// 	user.contrasenna !== document.querySelector('userForm__confirmPassword')
+	// ) {
+	// 	return mostrarMensajeError(
+	// 		'Las contraseñas a confirmar son diferentes. Inténtelo nuevamente'
+	// 	);
+	// }
+
 	let response;
 
 	console.log(user);
+
+	// En las peticiones fetch GET se debe de colocar la opcion url, creo que si es que se quiere especificar un puerto en específico, como el puerto 3000
 
 	fetch('http://127.0.0.1:3000/api/user/register', {
 		method: 'POST',
