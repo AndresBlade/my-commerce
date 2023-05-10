34a78a16461c93fc52ca7ca59e945c5dbffdded5
Alehernandezt <j.alehernandezt@gmail.com>
2023-05-10 15:38:35 -0400

    creo que listo amen

--- frontend/js/perfil.js
+++ frontend/js/perfil.js
@@ -11,11 +11,33 @@ document.addEventListener('DOMContentLoaded', () => {
 	document.querySelector('.userProfile__email').textContent = email;
 	document.querySelector('.userProfile__type').textContent = type;
 });
+//este codigo ya no sirve xd
+// var profilePic = document.getElementById("profile-pic");
+// var inputFile = document.getElementById("input-file");
+// inputFile.onchange = function(){
+// 	profilePic.src = URL.createObjectURL(inputFile.files[0])
+// }
 
+//amen
 var profilePic = document.getElementById("profile-pic");
-var inputFile = document.getElementById("input-file");
+profilePic.src = URL.createObjectURL(inputFile.files[0])
+const imagen = profilePic
 
-inputFile.onchange = function(){
-	profilePic.src = URL.createObjectURL(inputFile.files[0])
-}
+// const imagen = ui.inputImage.files[0];
+const formData = new FormData();
+
+formData.append('imagen', imagen);
+
+fetch('http://127.0.0.1:3000/api/tienda/register', {
+	mode: 'cors',
+	credentials: 'same-origin',
+	method: 'POST',
+	headers: {
+		Authorization: 'Bearer ' + token,
+	},
+	body: formData,
+}) .then(respuesta => {
+	respuesta.json();
+})
+
 
