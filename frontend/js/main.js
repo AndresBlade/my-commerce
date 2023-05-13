77fb10762b9d84c7a2fecaf9c8b615d177a686b8
Andres Arrieche <andresarrieche11@gmail.com>
2023-05-13 14:27:40 -0400

    Codigo refactorizado del main

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -30,34 +30,39 @@ menuToggle.onclick = function () {
 
 //---------------***** Index dinamico ***********----------------
 const boton_header = document.querySelector('.btn_sesion');
-const boton_perfil = document.querySelector('.btn_perfil');
 const boton_creaCuenta = document.querySelector('.btn-texto');
 const boton_creaCuentaLink = document.querySelector('.btnCrearCuenta');
+const modalSesion = document.querySelector('.modal__sesion');
+const btnSi = document.querySelector('.modal__si');
+const btnNo = document.querySelector('.modal__no');
 
 const dataJSON = localStorage.getItem('user');
 
 document.addEventListener('DOMContentLoaded', () => {
-	if (dataJSON) {
-		const data = JSON.parse(localStorage.getItem('user')).data;
-		const type = data.user.tipo_id === 1 ? 'Cliente' : 'Otro tipo rarito';
+	if (!dataJSON) return;
 
-		if (type === 'Cliente') {
-			console.log('hay una sesion abierta');
-			boton_header.textContent = 'Cerrar Sesión';
-			boton_creaCuenta.textContent = 'Registra tu tienda';
-			boton_perfil.style.display = 'block';
-
-			boton_creaCuentaLink.href =
-				'http://127.0.0.1/e-commerce-tarea/frontend/vistas/misTiendas.html';
-
-			boton_header.addEventListener('click', e => {
-				localStorage.removeItem('user');
-				boton_header.href =
-					'http://127.0.0.1/e-commerce-tarea/frontend/index.html';
-				location.reload();
-			});
-		}
+	if (boton_creaCuenta) {
+		boton_creaCuenta.textContent = 'Registra tu tienda';
+		boton_creaCuentaLink.href =
+			'http://127.0.0.1/e-commerce-tarea/frontend/vistas/misTiendas.html';
 	}
+
+	console.log('hay una sesion abierta');
+	boton_header.textContent = 'Cerrar Sesión';
+	boton_header.href = '#';
+
+	boton_header.addEventListener('click', e => {
+		modalSesion.classList.add('modal__sesion--show');
+		btnSi.addEventListener('click', e => {
+			localStorage.removeItem('user');
+			window.location.href = 'http://127.0.0.1/e-commerce-tarea/frontend';
+		});
+
+		btnNo.addEventListener('click', e => {
+			e.preventDefault();
+			modalSesion.classList.remove('modal__sesion--show');
+		});
+	});
 });
 
 //      perfil
