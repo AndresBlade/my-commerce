827559197ed0bc784499ade930c835c8785989bd
Andres Arrieche <andresarrieche11@gmail.com>
2023-05-13 15:58:54 -0400

    Se muestran scripts y modales de perfil y ayuda

--- frontend/js/ayuda.js
+++ frontend/js/ayuda.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const openModal = document.querySelector('#cajita1');
-const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.modal__close');
+const modal = document.querySelector('.modalSesion');
+const closeModal = document.querySelector('.modalSesion__close');
 
-openModal.addEventListener('click', (e)=>{
-    e.preventDefault();
-    console.log('sirvo');
-    modal.classList.add('modal--show');
+openModal.addEventListener('click', e => {
+	e.preventDefault();
+	console.log('sirvo');
+	modal.classList.add('modalSesion--show');
 });
 
-closeModal.addEventListener('click', (e)=>{
-    e.preventDefault();
-    modal.classList.remove('modal--show');
+closeModal.addEventListener('click', e => {
+	e.preventDefault();
+	modal.classList.remove('modalSesion--show');
 });
