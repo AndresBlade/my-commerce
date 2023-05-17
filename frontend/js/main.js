1b233e1a5a00ca4376032690dc9fccc31a3b4d33
Andres Arrieche <andresarrieche11@gmail.com>
2023-05-16 22:13:24 -0400

    Agregado los features al landing

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -7,6 +7,92 @@ let navigation = document.querySelector('.navigation');
 let menuToggle = document.querySelector('.menuToggle');
 let header = document.querySelector('.header');
 
+let productosGrid = document.querySelector('.tiendas-container');
+let tiendasGrid = document.querySelector('.grid');
+
+function fetchProducto(pageProduct = 0) {
+	return fetch(
+		`http://127.0.0.1:3000/api/productos/getProducts?page=${pageProduct}`,
+		{
+			method: 'GET',
+			url: 'http://127.0.0.1:3000',
+		}
+	)
+		.then(respuesta => {
+			if (!respuesta.ok)
+				return respuesta.text().then(texto => Promise.reject(texto));
+			return respuesta.json();
+		})
+		.catch(err => console.log(err));
+}
+
+function fetchTienda(pageTienda = 0, size = 6) {
+	return fetch(
+		`http://127.0.0.1:3000/api/tienda/tiendas?page=${pageTienda}&size=${size}`,
+		{
+			method: 'GET',
+			url: 'http://127.0.0.1:3000',
+		}
+	)
+		.then(respuesta => {
+			if (!respuesta.ok)
+				return respuesta.text().then(texto => Promise.reject(texto));
+			return respuesta.json();
+		})
+		.catch(err => console.log(err));
+}
+
+function renderizarProductos(productos) {
+	productosGrid.innerHTML = '';
+	productos.forEach(producto => {
+		const productoHTML = document.createElement('a');
+		productoHTML.style.textDecoration = 'none';
+		productoHTML.classList.add('producto');
+		const imagenes = producto.imagenes.split(' ');
+		const imagenHTML = `<img class="producto__imagen" src="${
+			imagenes[Math.floor(Math.random() * imagenes.length)]
+		}" />`;
+		productoHTML.innerHTML += imagenHTML;
+		productoHTML.innerHTML += `
+		<div class="informacion">
+		  <span class="tipo-envio">Envío Gratis</span>
+		  <span class="precio">${producto.precio}</span>
+		  <span class="precio-envio">Envío gratis</span>
+		  <span class="descripcion">${producto.nombre}</span>
+		</div>`;
+
+		const url = new URL(
+			'http://127.0.0.1/e-commerce-tarea/frontend/vistas/producto.html'
+		);
+		url.searchParams.set('id', producto.id);
+
+		productoHTML.href = url.href;
+		productosGrid.append(productoHTML);
+	});
+}
+
+function renderizarTiendas(tiendas) {
+	tiendasGrid.innerHTML = '';
+	tiendas.forEach(tienda => {
+		const tiendaHTML = document.createElement('a');
+		tiendaHTML.style.textDecoration = 'none';
+		tiendaHTML.classList.add('producto');
+		tiendaHTML.innerHTML += `
+		<img src="${tienda.imagen}" alt="" />
+		<div class="informacion">
+		<span class="tienda_nombre">${tienda.nombre}</span>
+		  <span class="descripcion">${tienda.descripcion}</span>
+		  <span class="ubicacion">Ir a ver</span>
+		</div>`;
+		const url = new URL(
+			'http://127.0.0.1/e-commerce-tarea/frontend/vistas/tienda.html'
+		);
+		url.searchParams.set('RIF', tienda.RIF);
+		tiendaHTML.href = url.href;
+		tiendasGrid.append(tiendaHTML);
+	});
+}
+
 searchBtn.onclick = function () {
 	searchBox.classList.add('active');
 	closeBtn.classList.add('active');
@@ -54,8 +140,6 @@ document.addEventListener('DOMContentLoaded', () => {
 		boton_creaCuentaLink.href =
 			'http://127.0.0.1/e-commerce-tarea/frontend/vistas/misTiendas.html';
 	}
-
-	console.log('hay una sesion abierta');
 	boton_header.textContent = 'Cerrar Sesión';
 	boton_header.href = '#';
 
@@ -71,27 +155,16 @@ document.addEventListener('DOMContentLoaded', () => {
 			modalSesion.classList.remove('modalSesion__sesion--show');
 		});
 	});
+	fetchProducto()
+		.then(productos => {
+			console.log(productos);
+			renderizarProductos(productos.products);
+		})
+		.catch(err => console.log(err));
+
+	fetchTienda()
+		.then(tiendas => {
+			renderizarTiendas(tiendas.tiendas);
+		})
+		.catch(err => console.log(err));
 });
-
-//      perfil
-// const toggle = document.querySelector(".toggle");
-// const menuDashboard = document.querySelector(".menu-dashboard");
-// const iconoMenu = toggle.querySelector("i");
-// const enlacesMenu = document.querySelectorAll(".enlace");
-
-// toggle.addEventListener("click", () => {
-//     menuDashboard.classList.toggle("open");
-
-//     if (iconoMenu.classList.contains("bx-menu")) {
-//         iconoMenu.classList.replace("bx-menu", "bx-x");
-//     } else {
-//         iconoMenu.classList.replace("bx-x", "bx-menu");
-//     }
-// });
-
-// enlacesMenu.forEach((enlace) => {
-//     enlace.addEventListener("click", () => {
-//         menuDashboard.classList.add("open");
-//         iconoMenu.classList.replace("bx-menu", "bx-x");
-//     });
-// });
