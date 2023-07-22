589291202d7d9a5462e12eaa6567a7abf37cc5b2
Andres Arrieche <andresarrieche11@gmail.com>
2023-07-21 20:47:07 -0400

    Agregados Links a tiendas del landing

--- mycommerce/src/shops/components/ShopCard.tsx
+++ mycommerce/src/shops/components/ShopCard.tsx
@@ -1,18 +1,17 @@
 import { Shop } from '../interfaces/Shop';
+import { Link } from 'react-router-dom';
 
 export const ShopCard = ({ RIF, imagen, nombre, descripcion }: Shop) => {
-	const url = new URL(
-		'http://127.0.0.1/e-commerce-tarea/frontend/vistas/tienda.html'
-	);
-	url.searchParams.set('RIF', RIF.toString());
+	// const url = new URL('localhost:5173/tienda.html');
+	// url.searchParams.set('RIF', RIF.toString());
 	return (
-		<a href={url.toString()} className="producto">
+		<Link to={`tienda/${RIF.toString()}`} className="producto">
 			<img src={imagen} alt="" />
 			<div className="informacion">
 				<span className="tienda_nombre">{nombre}</span>
 				<span className="descripcion">{descripcion}</span>
 				<span className="ubicacion">Ir a ver</span>
 			</div>
-		</a>
+		</Link>
 	);
 };
