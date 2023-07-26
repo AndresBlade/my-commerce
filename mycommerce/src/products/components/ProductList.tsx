9cffb57fa690861ff21d6a7ca09b17f9ef3d2ef6
Andres Arrieche <andresarrieche11@gmail.com>
2023-07-26 18:41:28 -0400

    limpiados comentarios en importaciones innecesarias en ProductLIst

--- mycommerce/src/products/components/ProductList.tsx
+++ mycommerce/src/products/components/ProductList.tsx
@@ -1,23 +1,8 @@
-import { useEffect, useState } from 'react';
-import { getProducts } from '../helpers/getProducts';
-import { Product } from '../interfaces/Product';
-import { ProductWrapper } from '../interfaces/ProductWrapper';
 import { ProductCard } from './ProductCard';
 import { useLoaderData } from 'react-router';
 import { LandingLoader } from '../../landing/interfaces/LandingLoader';
 
 export function ProductList() {
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars, prefer-const
-	// let [products, setProducts] = useState<Product[] | undefined>(undefined);
-
-	// useEffect(() => {
-	// 	getProducts()
-	// 		.then((response: ProductWrapper) => {
-	// 			setProducts(response.products);
-	// 		})
-	// 		.catch(error => console.log(error));
-	// }, []);
-
 	const { ProductsData } = useLoaderData() as LandingLoader;
 
 	return (
