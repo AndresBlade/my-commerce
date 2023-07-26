a835023ba86bbaf2fa34a61f9ee3c196849eac57
Andres Arrieche <andresarrieche11@gmail.com>
2023-07-26 18:42:22 -0400

    ShopList ahora está dentro de un grid, eliminados comentarios e importaciones innecesarios

--- mycommerce/src/shops/components/ShopList.tsx
+++ mycommerce/src/shops/components/ShopList.tsx
@@ -1,24 +1,13 @@
-import { useEffect, useState } from 'react';
-import { Shop } from '../interfaces/Shop';
-import { getShops } from '../helpers/getShops';
-import { ShopWrapper } from '../interfaces/ShopWrapper';
 import { ShopCard } from './ShopCard';
 import { useLoaderData } from 'react-router-dom';
 import { LandingLoader } from '../../landing/interfaces/LandingLoader';
 
 export const ShopList = () => {
 	const { ShopsData } = useLoaderData() as LandingLoader;
-	// const [shops, setShops] = useState<Shop[] | undefined>(undefined);
 
-	// useEffect(() => {
-	// 	getShops()
-	// 		.then((response: ShopWrapper) => {
-	// 			setShops(response.tiendas);
-	// 		})
-	// 		.catch(error => console.log(error));
-	// }, []);
+	console.log(useLoaderData());
 	return (
-		<div className="tiendas-container">
+		<div className="grid" id="gridTienda">
 			{ShopsData.tiendas?.map(shop => (
 				<ShopCard key={shop.RIF} {...shop} />
 			))}
