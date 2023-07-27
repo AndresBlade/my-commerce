7a745be063f68a93c131666c3ca2a1ba8c2a90b5
Andres Arrieche <andresarrieche11@gmail.com>
2023-07-27 18:38:08 -0400

    agregadas interfaces para informacion de las cartas y listas de ayuda

---
+++ mycommerce/src/help/interfaces/HelpCardInformation.ts
@@ -0,0 +1,4 @@
+export interface HelpCardInformation {
+	title: string;
+	description?: string;
+}
---
+++ mycommerce/src/help/interfaces/helpListInformation.ts
@@ -0,0 +1,6 @@
+import { HelpCardInformation } from './HelpCardInformation';
+
+export interface HelpListInformation {
+	heading: string;
+	helpCardInformation: HelpCardInformation[];
+}
