bb141e286f726b18ceb306d72c4562a6551d649e
Andres Arrieche <andresarrieche11@gmail.com>
2023-08-05 22:13:23 -0400

    Ahora se pueden comprar productos, con su respectiva animacion dependiendo del estado de la compra

--- mycommerce/src/products/components/ProductDetails.tsx
+++ mycommerce/src/products/components/ProductDetails.tsx
@@ -1,12 +1,31 @@
-import { useId } from 'react';
+import { useState } from 'react';
 import { SingleProduct } from '../interfaces/SingleProduct';
 import { Link } from 'react-router-dom';
+import { createPurchase } from '../../purchases/helpers/createPurchase';
+import { useContext } from 'react';
+import { AuthContext } from '../../auth/context/AuthContext';
+
+const handleSubmit = (
+	purchase: { producto_id: number; cantidad: number; precio: number },
+	token: string,
+	setPurchaseState: React.Dispatch<React.SetStateAction<boolean | undefined>>
+) => {
+	createPurchase(purchase, token)
+		.then(() => setPurchaseState(true))
+		.catch(() => setPurchaseState(false));
+};
+
 export const ProductDetails = ({
 	tienda,
 	nombre,
 	descripcion,
 	precio,
+	id,
 }: SingleProduct) => {
+	const [purchaseCompleted, setPurchaseCompleted] = useState<boolean>();
+	const {
+		userData: { token },
+	} = useContext(AuthContext);
 	return (
 		<article className="details">
 			<div className="container_tienda">
@@ -22,8 +41,31 @@ export const ProductDetails = ({
 			<h2 className="productName">{nombre}</h2>
 			<p className="descripcion">{descripcion}</p>
 			<p className="price">{precio}</p>
-			<button className="button compraProducto__btnComprar">
-				Comprar ahora
+			<button
+				className={`button compraProducto__btnComprar ${
+					purchaseCompleted === undefined
+						? ''
+						: purchaseCompleted
+						? 'compraProducto__btnComprar--comprado'
+						: 'compraProducto__btnComprar--error'
+				}`}
+				onClick={() =>
+					handleSubmit(
+						{
+							producto_id: id,
+							cantidad: 1,
+							precio: parseFloat(precio),
+						},
+						token,
+						setPurchaseCompleted
+					)
+				}
+			>
+				{purchaseCompleted === undefined
+					? 'Comprar Ahora'
+					: purchaseCompleted
+					? 'Comprado exitosamente!'
+					: 'Error al comprar'}
 			</button>
 		</article>
 	);
---
+++ mycommerce/src/purchases/helpers/createPurchase.ts
@@ -0,0 +1,23 @@
+export const createPurchase = (
+	purchase: {
+		producto_id: number;
+		cantidad: number;
+		precio: number;
+	},
+	token: string
+): Promise<unknown> =>
+	fetch('http://127.0.0.1:3000/api/ventas_detalle/individualPurchase', {
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + token,
+		},
+		body: JSON.stringify(purchase),
+	}).then(response => {
+		if (!response.ok) {
+			throw new Error(response.statusText);
+		}
+		return response.json() as Promise<unknown>;
+	});
