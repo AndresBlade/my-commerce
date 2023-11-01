16a1ff9a93ebced0869bccc939886f127fb029ea
Andres Arrieche <andresarrieche11@gmail.com>
2023-10-31 22:44:10 -0400

    fetch mejorado

--- frontend/src/admin/helpers/getLogBody.ts
+++ frontend/src/admin/helpers/getLogBody.ts
@@ -1,11 +1,13 @@
 export function getLogBody(token: string, username: string): Promise<Blob> {
-	return fetch(`http://127.0.0.1:3000/api/bitacora/getAllUserLogFiles`, {
-		method: 'GET',
-		headers: {
-			Authorization: 'Bearer ' + token,
-		},
-		body: JSON.stringify({ username }),
-	}).then(response => {
+	return fetch(
+		`http://127.0.0.1:3000/api/bitacora/downloadUserLogFile/${username}`,
+		{
+			method: 'GET',
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		}
+	).then(response => {
 		if (!response.ok) {
 			throw new Error(response.statusText);
 		}
