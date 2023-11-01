79693fce92b5e977a98dcf5e890ea65eb13f8bfc
Cesar <cmmh200427@gmail.com>
2023-10-31 23:44:40 -0400

    intento de createCategories

--- frontend/src/admin/pages/CrearCategoriasPage.tsx
+++ frontend/src/admin/pages/CrearCategoriasPage.tsx
@@ -1,13 +1,27 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../auth/context/AuthContext';
 import { getCategories } from "../../categories/helpers/getCategories";
 import { createCategory } from "../../categories/helpers/createCategories";
 import { updateCategory } from "../../categories/helpers/updateCategories";
 
-
 export const CrearCategoriasPage = () => {
+	const {
+		user: { token },
+	} = useContext(AuthContext);
 
-	return (
-		<div>
+  const categoryName = 'Biblioteca';
+  
+  createCategory(categoryName, token)
+    .then(data => {
+      console.log('Categoría creada con éxito:', data);
+    })
+    .catch(error => {
+      console.error('Hubo un error al crear la categoría:', error);
+    });
 
-		</div>
-	);
+  return (
+    <div>
+
+    </div>
+  );
 };
--- frontend/src/categories/helpers/createCategories.ts
+++ frontend/src/categories/helpers/createCategories.ts
@@ -1,16 +1,18 @@
-
-export function createCategory(formData: FormData, token: string) {
-	return fetch(`http://127.0.0.1:3000/api/categorias/createCategory`, {
-		method: 'POST',
-		headers: {
-			Authorization: 'Bearer ' + token,
-		},
-		body: formData,
-	}).then(response => {
+export function createCategory(categoryName:string, token:string) {
+  
+	return fetch('http://127.0.0.1:3000/api/categorias/createCategory', {
+	  method: 'POST',
+	  headers: {
+		'Authorization': 'Bearer ' + token,
+		'Content-Type': 'application/json'
+	  },
+	  body: JSON.stringify(categoryName)
+	})
+	  .then(response => {
 		if (!response.ok) {
-			throw new Error(response.statusText);
+		  throw new Error(response.statusText);
 		}
 		return response.json();
-	});
-}
-
+	  });
+  }
+  
